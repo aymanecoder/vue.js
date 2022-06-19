676ff40bb349a542898007aa0ec6996f13192a9f
Aymane Biti <93627479+aymanecoder@users.noreply.github.com>
2022-06-18 18:27:39 -0700

    sums

---
+++ src/components/Pet.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="pet">
+        <!-- <h3>{{ pet.name }}</h3> -->
+        <!-- <Button label="Save" /> -->
+        <Card>
+            <template #header>
+                <img :src="pet.url" />
+            </template>
+            <template #title>
+                {{ pet.name }}
+            </template>
+            <template #subtitle>
+                Age: {{ pet.age }}
+            </template>
+            <!-- <template #content>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error
+                    repudiandae numquam deserunt
+                    quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate
+                    neque quas!</p>
+            </template> -->
+            <template #footer>
+
+                <Button @click="OnRemove(pet.id)"   icon="pi pi-times" label="Remove" class="p-button-danger" style="margin-right: 4em" />
+                <Button @click="$emit('add-favorite',pet.id)" icon="pi pi-heart"
+                    :class="[pet.isFavorite ? '' : 'p-button-outlined', 'p-button-rounded', 'p-button-danger']" />
+
+            </template>
+        </Card>
+    </div>
+</template>
+
+
+<script>
+export default {
+    props:
+    {
+
+        pet: Object,
+    },
+    methods:{
+        OnRemove(id){
+           this.$emit("remove-pet",id);
+
+        }
+    }
+}
+
+</script>
+<style>
+</style>
---
+++ src/components/Pets.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="grid">
+        <div class="col-12 md:col-6 lg:col-3" v-for="pet in pets" :key="pet.id">
+            <!-- <h3>{{pet.name}}</h3> -->
+            <Pet @remove-pet="$emit('remove-pet',pet.id)" @add-favorite="$emit('add-favorite',pet.id)" :pet="pet" />
+
+        </div>
+    </div>
+</template>
+
+<script>
+import Pet from '../components/Pet'
+export default {
+    props: {
+        pets: Array
+    },
+    components: {
+        Pet
+    },
+    emits:["remove-pet","add-favorite"],
+
+}
+</script>
+
+<style>
+</style>
